b66a373bc8ab0b380e521fe93eed69bfac960d36
marwinburesch <marwin@posteo.de>
2024-08-19 15:31:23 +0200

    ✨ Implement example Entity Component System

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,38 +1,106 @@
 import { Scene } from "phaser";
+import {
+  createWorld,
+  addEntity,
+  addComponent,
+  defineComponent,
+  defineSystem,
+  defineQuery,
+  enterQuery,
+  System,
+  IWorld,
+  Types,
+  exitQuery,
+} from "bitecs";
+
+const Position = defineComponent({
+  x: Types.f32,
+  y: Types.f32,
+});
+
+const Velocity = defineComponent({
+  x: Types.f32,
+  y: Types.f32,
+});
+
+const Sprite = defineComponent({
+  texture: Types.ui8,
+});
+
+const spritesById = new Map<number, Phaser.GameObjects.Sprite>();
+const spriteQuery = defineQuery([Sprite, Position]);
+const spriteQueryEnter = enterQuery(spriteQuery);
+const spriteQueryExit = exitQuery(spriteQuery);
+const createSpriteSystem = (scene: Scene, textures: string[]) => {
+  return defineSystem((world) => {
+    const enterEntities = spriteQueryEnter(world);
+    for (let i = 0; i < enterEntities.length; i++) {
+      const id = enterEntities[i];
+      const texId = Sprite.texture[id];
+      const texture = textures[texId];
+      spritesById.set(id, scene.add.sprite(0, 0, texture));
+    }
+    const entities = spriteQuery(world);
+    for (let i = 0; i < entities.length; i++) {
+      const id = entities[i];
+      const sprite = spritesById.get(id);
+      if (!sprite) {
+        continue;
+      }
+      sprite.x = Position.x[id];
+      sprite.y = Position.y[id];
+    }
+
+    const exitEntities = spriteQueryExit(world);
+    for (let i = 0; i < exitEntities.length; i++) {
+      const id = exitEntities[i];
+      const sprite = spritesById.get(id);
+      if (!sprite) {
+        continue;
+      }
+      sprite.destroy();
+      spritesById.delete(id);
+    }
+
+    return world;
+  });
+};
 
 export class Game extends Scene {
-  camera: Phaser.Cameras.Scene2D.Camera;
-  background: Phaser.GameObjects.Image;
-  msg_text: Phaser.GameObjects.Text;
+  private world?: IWorld;
+  private spriteSystem?: System;
 
   constructor() {
     super("Game");
   }
 
+  preload() {
+    this.load.image("miner", "assets/character/idle-front.png");
+    this.load.image("bat", "assets/enemies/bat/fly-front.png");
+  }
+
   create() {
-    this.camera = this.cameras.main;
-    this.camera.setBackgroundColor(0x00ff00);
+    this.world = createWorld();
+    const miner = addEntity(this.world);
 
-    this.background = this.add.image(512, 384, "background");
-    this.background.setAlpha(0.5);
+    addComponent(this.world, Position, miner);
+    Position.x[miner] = 100;
+    Position.y[miner] = 100;
 
-    this.msg_text = this.add.text(
-      512,
-      384,
-      "Make something fun!\nand share it with us:\nsupport@phaser.io",
-      {
-        fontFamily: "Arial Black",
-        fontSize: 38,
-        color: "#ffffff",
-        stroke: "#000000",
-        strokeThickness: 8,
-        align: "center",
-      }
-    );
-    this.msg_text.setOrigin(0.5);
+    addComponent(this.world, Velocity, miner);
+    Velocity.x[miner] = 5;
+    Velocity.y[miner] = 5;
 
-    this.input.once("pointerdown", () => {
-      this.scene.start("Level");
-    });
+    addComponent(this.world, Sprite, miner);
+    Sprite.texture[miner] = 0;
+
+    this.spriteSystem = createSpriteSystem(this, ["miner", "bat"]);
+  }
+
+  update(t: number, dt: number): void {
+    if (!this.world || !this.spriteSystem) {
+      return;
+    }
+    this.spriteSystem(this.world);
   }
 }
