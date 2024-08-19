2bcd93a2981d4f5c9d284a484ba251aa6314f50c
marwinburesch <marwin@posteo.de>
2024-08-19 15:56:42 +0200

    ✨ Add another character

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -82,17 +82,26 @@ export class Game extends Scene {
   create() {
     this.world = createWorld();
     const miner = addEntity(this.world);
+    const bat = addEntity(this.world);
 
     addComponent(this.world, Position, miner);
     Position.x[miner] = 100;
     Position.y[miner] = 100;
+    addComponent(this.world, Position, bat);
+    Position.x[bat] = 200;
+    Position.y[bat] = 200;
 
     addComponent(this.world, Velocity, miner);
     Velocity.x[miner] = 5;
     Velocity.y[miner] = 5;
+    addComponent(this.world, Velocity, bat);
+    Velocity.x[bat] = 5;
+    Velocity.y[bat] = 5;
 
     addComponent(this.world, Sprite, miner);
     Sprite.texture[miner] = 0;
+    addComponent(this.world, Sprite, bat);
+    Sprite.texture[bat] = 1;
 
     this.spriteSystem = createSpriteSystem(this, ["miner", "bat"]);
   }
